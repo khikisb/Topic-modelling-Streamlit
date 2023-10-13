eb8b14c859dafe8869c640f1cab0fa5351577dc1
khikisb <89850965+khikisb@users.noreply.github.com>
2023-10-13 15:08:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,9 @@ with Model:
     lda = LatentDirichletAllocation(n_components=topik, doc_topic_prior=0.2, topic_word_prior=0.1, random_state=42, max_iter=1)
     lda_top = lda.fit_transform(tf)
     data_with_lda = pd.concat([tf, data['Label']], axis=1)
-    y = data_with_lda.Label
+   
+    df = data_with_lda.dropna()
+    y = df.Label
     X_train, X_test, y_train, y_test = train_test_split(lda_top, y, test_size=0.2, random_state=42)
     
     metode1 = KNeighborsClassifier(n_neighbors=3)
