689e6852405cd500cc2cd034013061abc6d8b171
khikisb <89850965+khikisb@users.noreply.github.com>
2023-10-13 17:42:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -131,7 +131,14 @@ with Implementasi:
    y = df['Label'].values
    labels = y
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-   # Contoh penggunaan:
+
+   from sklearn.feature_extraction.text import CountVectorizer
+
+   # Inisialisasi CountVectorizer
+   vectorizer = CountVectorizer()
+   
+   # Menggunakan CountVectorizer pada data pelatihan
+   X_train_vectorized = vectorizer.fit_transform(X_train)
    # Inisialisasi model KNN
    knn = KNeighborsClassifier(n_neighbors=5)
    knn.fit(X_train, y_train)
