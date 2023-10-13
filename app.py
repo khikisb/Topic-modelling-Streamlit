4c2d791c4bc52497310974cfa44dc93db105339c
khikisb <89850965+khikisb@users.noreply.github.com>
2023-10-13 15:03:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ with Model:
     st.write("Jika pada menu LDA tidak menentukan jumlah topiknya maka proses modelling akan di default dengan jumlah topik = 1")
     lda = LatentDirichletAllocation(n_components=topik, doc_topic_prior=0.2, topic_word_prior=0.1, random_state=42, max_iter=1)
     lda_top = lda.fit_transform(tf)
-    y = data_with_lda['Label']
+    y = data.label
     X_train, X_test, y_train, y_test = train_test_split(lda_top, y, test_size=0.2, random_state=42)
     
     metode1 = KNeighborsClassifier(n_neighbors=3)
