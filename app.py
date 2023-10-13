ce623f4179bb70812ed2595e50e2ff0c013ca23d
khikisb <89850965+khikisb@users.noreply.github.com>
2023-10-13 17:28:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -119,26 +119,27 @@ with Implementasi:
        # Memprediksi label untuk input pengguna
        predicted_label = knn.predict(user_input_vector)
        return predicted_label[0]
-
-   tf = pd.read_csv("df_tf.csv")
-   st.subheader("Jumlah Topik yang Anda Gunakan : " + str(topik))
-   st.write("Jika pada menu LDA tidak menentukan jumlah topiknya maka proses modelling akan di default dengan jumlah topik = 1")
-   lda = LatentDirichletAllocation(n_components=topik, doc_topic_prior=0.2, topic_word_prior=0.1, random_state=42, max_iter=1)
-   lda_top = lda.fit_transform(tf)
-   data_with_lda = pd.concat([tf, data['Label']], axis=1)
    
-   df = data_with_lda.dropna(subset=['Label', 'Label'])
-
+   # Membaca data dari file CSV
+   df = pd.read_csv("df_tf.csv")
+   
+   # Inisialisasi model LDA
+   topik = 10  # Ganti dengan jumlah topik yang sesuai
+   lda = LatentDirichletAllocation(n_components=topik, doc_topic_prior=0.2, topic_word_prior=0.1, random_state=42, max_iter=1)
+   lda_top = lda.fit_transform(df)
+   
+   # Memisahkan data menjadi fitur (X) dan label (y)
    X = df.drop(columns=['Label']).values
    y = df['Label'].values
-   labels = y
+   
+   # Memisahkan data menjadi data pelatihan dan data pengujian
    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-   # Contoh penggunaan:
+   
    # Inisialisasi model KNN
    knn = KNeighborsClassifier(n_neighbors=5)
-   knn.fit(X[1:], labels)
+   knn.fit(X_train, y_train)
+   
+   # Contoh penggunaan:
    user_input = "Abstrak baru yang ingin diprediksi"
    predicted_label = predict_label(user_input, lda, knn, vectorizer)
    print(f"Label yang diprediksi: {predicted_label}")
-
-
