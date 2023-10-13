ae071cb7f717c222974f91a8766887cc6f12cd23
khikisb <89850965+khikisb@users.noreply.github.com>
2023-10-13 13:32:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,118 +1,76 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-from numpy import array
-import pickle
 from sklearn.decomposition import LatentDirichletAllocation
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import tree
 
-Data, lda, Model = st.tabs(['Data', 'LDA', 'Modelling'])
+st.title("UTS Pencarian & Penambangan Web A")
+st.text("Okhi Sahrul Barkah")
 
-with Data :
-   st.title("""UTS Pencarian & Penambangan Web A""")
-   st.text('Okhi Sahrul Barkah')
-   st.subheader('Deskripsi Data')
-   st.write("""Dimana Fitur yang ada di dalam data tersebut diantaranya :""")
-   st.text("""
-            1) Judul
-            2) Penulis
-            3) Dosen Pembimbing 1
-            4) Dosen Pembinbing 2
-            5) Abstrak
-            5) Label""")
-   st.subheader('Data')
-   data=pd.read_csv('DF_PTA_LABEL.csv')
-   data
+Data, lda, Model = st.beta_columns(3)
 
-# with Ekstraksi :
-#    st.subheader('Term Frequency (TF)')
-#    df_tf = pd.read_csv('df_tf.csv')
-#    df_tf
-   
-#    st.subheader('Logarithm Frequency (Log-TF)')
-#    df_log = pd.read_csv('df_log.csv')
-#    df_log
-   
-#    st.subheader('Binary Frequency')
-#    binary_data = pd.read_csv('binary_data.csv')
-#    binary_data
-   
-#    st.subheader('TF-IDF')
-#    df_tf_idf = pd.read_csv('df_tf_idf.csv')
-#    df_tf_idf
+with Data:
+    st.subheader("Deskripsi Data")
+    st.write("Dimana Fitur yang ada di dalam data tersebut diantaranya :")
+    st.text("1) Judul\n2) Penulis\n3) Dosen Pembimbing 1\n4) Dosen Pembinbing 2\n5) Abstrak\n6) Label")
+    st.subheader("Data")
+    data = pd.read_csv("DF_PTA_LABEL.csv")
+    st.write(data)
 
 with lda:
-   topik = st.number_input("Masukkan Jumlah Topik yang Diinginkan", 1, step=1)
-   
-   def submit():
-       lda = LatentDirichletAllocation(n_components=topik, doc_topic_prior=0.2, topic_word_prior=0.1, random_state=42)
-       df_tf = pd.read_csv('TF_label.csv')
-       x = df_tf.drop('Label', axis=1)
-       lda_top = lda.fit_transform(x)
-       
-       # Bobot setiap topik terhadap dokumen
-       nama_clm = []
-       for i in range(topik):
-           nama_clm.append("Topik " + str(i+1))
-       U = pd.DataFrame(lda_top, columns=nama_clm)
-       
-       # Menggunakan label dari DataFrame yang sesuai
-       U['Label'] = df_tf['Label'].values
-       
-       st.write(U)
-       submit()
+    topik = st.number_input("Masukkan Jumlah Topik yang Diinginkan", 1, step=1)
 
-with Model :
-    # if all :
-        st.subheader('Jumlah Topik yang Anda Gunakan : ' +str(topik))
-        st.write ("Jika pada menu LDA tidak menentukan jumlah topiknya maka proses modelling akan di default dengan jumlah topik = 1")
-        lda = LatentDirichletAllocation(n_components=topik, doc_topic_prior=0.2, topic_word_prior=0.1,random_state=42,max_iter=1)
-        x = df_tf.drop(columns='Label')
+    def submit():
+        df_tf = pd.read_csv("TF_label.csv")
+        lda = LatentDirichletAllocation(n_components=topik, doc_topic_prior=0.2, topic_word_prior=0.1, random_state=42, max_iter=1)
+        x = df_tf.drop("Label", axis=1)
         lda_top = lda.fit_transform(x)
-        y = df_tf.Label
-        X_train,X_test,y_train,y_test = train_test_split(lda_top,y,test_size=0.2,random_state=42)
-        
-        metode1 = KNeighborsClassifier(n_neighbors=3)
-        metode1.fit(X_train, y_train)
+        nama_clm = [f"Topik {i+1}" for i in range(topik)]
+        U = pd.DataFrame(lda_top, columns=nama_clm)
+        U["Label"] = df_tf["Label"].values
+        st.write(U)
 
-        metode2 = GaussianNB()
-        metode2.fit(X_train, y_train)
+    if st.button("Submit"):
+        st.balloons()
+        submit()
 
-        metode3 = tree.DecisionTreeClassifier(criterion="gini")
-        metode3.fit(X_train, y_train)
+with Model:
+    st.subheader("Jumlah Topik yang Anda Gunakan : " + str(topik))
+    st.write("Jika pada menu LDA tidak menentukan jumlah topiknya maka proses modelling akan di default dengan jumlah topik = 1")
+    lda = LatentDirichletAllocation(n_components=topik, doc_topic_prior=0.2, topic_word_prior=0.1, random_state=42, max_iter=1)
+    x = df_tf.drop(columns="Label")
+    lda_top = lda.fit_transform(x)
+    y = df_tf["Label"]
+    X_train, X_test, y_train, y_test = train_test_split(lda_top, y, test_size=0.2, random_state=42)
 
-        st.write ("Pilih metode yang ingin anda gunakan :")
-        met1 = st.checkbox("KNN")
-        # if met1 :
-        #     st.write("Hasil Akurasi Data Training Menggunakan KNN sebesar : ", (100 * metode1.score(X_train, y_train)))
-        #     st.write("Hasil Akurasi Data Testing Menggunakan KNN sebesar : ", (100 * (metode1.score(X_test, y_test))))
-        met2 = st.checkbox("Naive Bayes")
-        # if met2 :
-        #     st.write("Hasil Akurasi Data Training Menggunakan Naive Bayes sebesar : ", (100 * metode2.score(X_train, y_train)))
-        #     st.write("Hasil Akurasi Data Testing Menggunakan Naive Bayes sebesar : ", (100 * metode2.score(X_test, y_test)))
-        met3 = st.checkbox("Decesion Tree")
-        # if met3 :
-            # st.write("Hasil Akurasi Data Training Menggunakan Decission Tree sebesar : ", (100 * metode3.score(X_train, y_train)))
-            # st.write("Hasil Akurasi Data Testing Menggunakan Decission Tree sebesar : ", (100 * metode3.score(X_test, y_test)))
-        submit2 = st.button("Pilih")
+    metode1 = KNeighborsClassifier(n_neighbors=3)
+    metode1.fit(X_train, y_train)
 
-        if submit2:      
-            if met1 :
-                st.write("Metode yang Anda gunakan Adalah KNN")
-                st.write("Hasil Akurasi Data Training Menggunakan KNN sebesar : ", (100 * metode1.score(X_train, y_train)))
-                st.write("Hasil Akurasi Data Testing Menggunakan KNN sebesar : ", (100 * (metode1.score(X_test, y_test))))
-            elif met2 :
-                st.write("Metode yang Anda gunakan Adalah Naive Bayes")
-                st.write("Hasil Akurasi Data Training Menggunakan Naive Bayes sebesar : ", (100 * metode2.score(X_train, y_train)))
-                st.write("Hasil Akurasi Data Testing Menggunakan Naive Bayes sebesar : ", (100 * metode2.score(X_test, y_test)))
-            elif met3 :
-                st.write("Metode yang Anda gunakan Adalah Decesion Tree")
-                st.write("Hasil Akurasi Data Training Menggunakan Decission Tree sebesar : ", (100 * metode3.score(X_train, y_train)))
-                st.write("Hasil Akurasi Data Testing Menggunakan Decission Tree sebesar : ", (100 * metode3.score(X_test, y_test)))
-            else :
-                st.write("Anda Belum Memilih Metode")
-    # else:
-    #     st.write("Anda Belum Menentukan Jumlah Topik di Menu LDA")
+    metode2 = GaussianNB()
+    metode2.fit(X_train, y_train)
+
+    metode3 = tree.DecisionTreeClassifier(criterion="gini")
+    metode3.fit(X_train, y_train)
+
+    st.write("Pilih metode yang ingin anda gunakan :")
+    met1 = st.checkbox("KNN")
+    met2 = st.checkbox("Naive Bayes")
+    met3 = st.checkbox("Decision Tree")
+
+    if st.button("Pilih"):
+        if met1:
+            st.write("Metode yang Anda gunakan Adalah KNN")
+            st.write("Hasil Akurasi Data Training Menggunakan KNN sebesar:", 100 * metode1.score(X_train, y_train))
+            st.write("Hasil Akurasi Data Testing Menggunakan KNN sebesar:", 100 * metode1.score(X_test, y_test))
+        elif met2:
+            st.write("Metode yang Anda gunakan Adalah Naive Bayes")
+            st.write("Hasil Akurasi Data Training Menggunakan Naive Bayes sebesar:", 100 * metode2.score(X_train, y_train))
+            st.write("Hasil Akurasi Data Testing Menggunakan Naive Bayes sebesar:", 100 * metode2.score(X_test, y_test))
+        elif met3:
+            st.write("Metode yang Anda gunakan Adalah Decision Tree")
+            st.write("Hasil Akurasi Data Training Menggunakan Decision Tree sebesar:", 100 * metode3.score(X_train, y_train))
+            st.write("Hasil Akurasi Data Testing Menggunakan Decision Tree sebesar:", 100 * metode3.score(X_test, y_test))
+        else:
+            st.write("Anda Belum Memilih Metode")
