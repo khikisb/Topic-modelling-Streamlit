3d3f225154857abb01869721e5eca98b40cd91e6
khikisb <89850965+khikisb@users.noreply.github.com>
2023-10-13 15:11:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -40,11 +40,12 @@ with Model:
     tf = pd.read_csv("df_tf.csv")
     st.subheader("Jumlah Topik yang Anda Gunakan : " + str(topik))
     st.write("Jika pada menu LDA tidak menentukan jumlah topiknya maka proses modelling akan di default dengan jumlah topik = 1")
-    lda = LatentDirichletAllocation(n_components=topik, doc_topic_prior=0.2, topic_word_prior=0.1, random_state=42, max_iter=1)
-    lda_top = lda.fit_transform(tf)
     data_with_lda = pd.concat([tf, data['Label']], axis=1)
    
     df = data_with_lda.dropna()
+
+    lda = LatentDirichletAllocation(n_components=topik, doc_topic_prior=0.2, topic_word_prior=0.1, random_state=42, max_iter=1)
+    lda_top = lda.fit_transform(df.drop(columns=['Label']).values)
     y = df.Label
     X_train, X_test, y_train, y_test = train_test_split(lda_top, y, test_size=0.2, random_state=42)
     
